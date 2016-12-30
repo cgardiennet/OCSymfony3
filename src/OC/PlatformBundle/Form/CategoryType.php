9dc713f6162df734e7e042bf957d45d2e0891bc2
Cédric GARDIENNET <c.gardiennet@training-orchestra.com>
2016-12-30 15:17:17 +0100

    ref #OC-chapter22 Modifications on form class CategoryType

--- src/OC/PlatformBundle/Form/CategoryType.php
+++ src/OC/PlatformBundle/Form/CategoryType.php
@@ -3,7 +3,7 @@
 namespace OC\PlatformBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Form;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -13,10 +13,7 @@ class CategoryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add(
-                'name',
-                ChoiceType::class
-            )
+            ->add('name', TextType::class)
         ;
 
     }
